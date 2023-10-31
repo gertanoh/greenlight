4ed73c69ad1c11cfd6e80ce5bfb063be12392389
dev-station <thenry702@gmail.com>
2023-10-31 08:01:45 +0100

    Clean up comments

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -16,8 +16,6 @@ import (
 
 type envelope map[string]interface{}
 
-// Retrieve the "id" URL parameter from the current request context, then convert it to
-// an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -27,9 +25,6 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-// Define a writeJSON() helper for sending responses. This takes the destination
-// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
-// header map containing any additional HTTP headers we want to include in the response.
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
 	js, err := json.MarshalIndent(data, "", "\t")
@@ -38,11 +33,6 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	}
 	// Append a newline to make it easier to view in terminal applications.
 	js = append(js, '\n')
-	// At this point, we know that we won't encounter any more errors before writing the
-	// response, so it's safe to add any headers that we want to include. We loop
-	// through the header map and add each header to the http.ResponseWriter header map.
-	// Note that it's OK if the provided header map is nil. Go doesn't throw an error
-	// if you try to range over (or generally, read from) a nil map.
 	for key, value := range headers {
 		w.Header()[key] = value
 	}
@@ -62,7 +52,6 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	dec.DisallowUnknownFields()
 	err := dec.Decode(dst)
 	if err != nil {
-		// If there is an error during decoding, start the triage...
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
