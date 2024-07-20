788c717f7d8d94b0f53cd00898fc8fb14d18cdd5
dev-station-wsl-ubuntu-22 <thenry702@gmail.com>
2024-07-20 09:11:27 +0200

    add kubernetes depoyment

---
+++ deploy/Readme.md
@@ -0,0 +1,14 @@
+# Transitiong from Docker compose to K8S
+
+## List
+
+- postgres : using bitnami official image and applying a specific values files
+
+- redis : same  as above : done
+
+- migrate
+
+- prometheus / grafana : TODO
+
+- web / nginx : nginx is not useful anymore
+
---
+++ deploy/helm_install_postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+set -o allexport
+source ../.env
+set +o allexport
+
+helm uninstall postgres
+kubectl delete pvc data-greenlight-db-postgresql-0
+
+helm install greenlight-db bitnami/postgresql -f postgres-values.yaml \
+  --set global.postgresql.auth.username=$POSTGRES_USER \
+  --set global.postgresql.auth.password=$POSTGRES_PASSWORD \
+  --set global.postgresql.auth.database=$POSTGRES_DB
